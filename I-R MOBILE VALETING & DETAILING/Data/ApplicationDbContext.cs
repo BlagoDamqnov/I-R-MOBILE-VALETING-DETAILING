7b172edbfa617380cefb66e9ed56c3c6fd6d9990
Blagovest Damyanov <91145357+BlagoDamqnov@users.noreply.github.com>
2023-08-13 13:46:38 +0300

    Add project files.

---
+++ I-R MOBILE VALETING & DETAILING/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace I_R_MOBILE_VALETING___DETAILING.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ I-R MOBILE VALETING & DETAILING/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace I_R_MOBILE_VALETING___DETAILING.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
