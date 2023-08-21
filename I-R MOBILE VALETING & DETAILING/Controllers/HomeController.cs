0e8136a5c3a552d073392a2e9b26ea0492c35bda
Blagovest Damyanov <91145357+BlagoDamqnov@users.noreply.github.com>
2023-08-21 17:01:31 +0300

    Test update

--- I-R MOBILE VALETING & DETAILING/Controllers/HomeController.cs
+++ I-R MOBILE VALETING & DETAILING/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using I_R_MOBILE_VALETING___DETAILING.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace I_R_MOBILE_VALETING___DETAILING.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
